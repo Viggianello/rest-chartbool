1c3bdea005fcd234758a869ff7aa09fa38e83fc4
Antonello Viggiano <antonelloviggiano1990@hotmail.it>
2020-06-12 11:55:59 +0200

    working progress

--- main.js
+++ main.js
@@ -16,53 +16,88 @@ $(document).ready(function() {
     var url_api= 'http://157.230.17.132:'+ numero_porta_personale +'/sales';
     // imposto la lingua italiana per moment
     moment.locale('it');
-	//Chiamata ajax
-    $.ajax({
-        // qui parte l'oggetto dei dischi dieci musicali
-        'url': url_api,
-        'method': 'GET',
-        'success': function(vendite) {
-            console.log(vendite);
-            /* grafico vendite mensili */
-            var dati_vendite_mensili = prepara_dati_vendite_mensili(vendite);
-            var mesi = Object.keys(dati_vendite_mensili);
-            var dati_mesi = Object.values(dati_vendite_mensili);
-            disegna_grafico_vendite_mensili(mesi, dati_mesi);
 
-            /* grafico vendite venditore */
-            // costruisco un oggetto che mappa i venditori con il totale delle vendite
-            var dati_vendite_venditori = prepara_dati_vendite_venditori(vendite);
-            // estraggo le chiavi, che saranno le etichette del grafico
-            var nomi_venditori = Object.keys(dati_vendite_venditori);
-            // estraggo i valori, che saranno i dati del grafico
-            var dati_venditori = Object.values(dati_vendite_venditori);
-            // disegno il grafico passandogli le etichette e i dati
-            disegna_grafico_vendite_venditori(nomi_venditori, dati_venditori);
-        },
-        'error': function() {
-            console.log('si è verificato un errore');
-        }// fine oggetto
-    });// fine chiamata ajax
+    // aggancio funzione click a bottone
+    $('#aggiungi-vendita').click(function() {
+        var venditore_scelto =$ ('#scelta-venditore').val();
+        var mese_scelto =$('#scelta-mese').val();
+        var importo_inserito = $('#importo-vendita').val();
+        var data_moment= moment(mese_scelto, 'MMMM');
+        var mese = data_moment.format('MM');
+        var data_vendita = '01/' + mese +'/2017';
+
+        disegna_grafici();
+        //Chiamata ajax
+        $.ajax({
+            'url': url_api,
+            'method': 'POST',
+            'data':{
+                salesman: venditore_scelto,
+                date: data_vendita,
+                amount: importo_inserito
+                // lo metterà come stringa per quello fareme il parseInt quando lo leggeremo
+            },
+            'success': function() {
+                disegna_grafici();
+            },
+            'error': function() {
+                console.log('si è verificato un errore');
+            }
+        })
+    })
+
+    function disegna_grafici() {
+        $.ajax({
+            'url': url_api,
+            'method': 'GET',
+            'success': function(vendite) {
+                console.log(vendite);
+                /* grafico vendite mensili */
+                var dati_vendite_mensili = prepara_dati_vendite_mensili(vendite);
+                var mesi = Object.keys(dati_vendite_mensili);
+                var dati_mesi = Object.values(dati_vendite_mensili);
+                disegna_grafico_vendite_mensili(mesi, dati_mesi);
+
+                /* grafico vendite venditore */
+                // costruisco un oggetto che mappa i venditori con il totale delle vendite
+                var dati_vendite_venditori = prepara_dati_vendite_venditori(vendite);
+                // estraggo le chiavi, che saranno le etichette del grafico
+                var nomi_venditori = Object.keys(dati_vendite_venditori);
+                // estraggo i valori, che saranno i dati del grafico
+                var dati_venditori = Object.values(dati_vendite_venditori);
+                // disegno il grafico passandogli le etichette e i dati
+                disegna_grafico_vendite_venditori(nomi_venditori, dati_venditori);
+            },
+            'error': function() {
+                console.log('si è verificato un errore');
+            }// fine oggetto
+        });// fine chiamata ajax
+
+    }
 
     function prepara_dati_vendite_mensili(dati) {
         var vendite_mensili ={
-            'gennaio': 0,
-            'febbraio': 0,
-            'marzo': 0,
-            'aprile': 0,
-            'maggio': 0,
-            'giugno': 0,
-            'luglio': 0,
-            'agosto': 0,
-            'settembre': 0,
-            'ottobre': 0,
-            'novembre': 0,
-            'dicembre': 0,
+            // 'gennaio': 0,
+            // 'febbraio': 0,
+            // 'marzo': 0,
+            // 'aprile': 0,
+            // 'maggio': 0,
+            // 'giugno': 0,
+            // 'luglio': 0,
+            // 'agosto': 0,
+            // 'settembre': 0,
+            // 'ottobre': 0,
+            // 'novembre': 0,
+            // 'dicembre': 0,
+        }
+        for (var i = 1; i <= 12; i++) {
+            var nome_mese = moment(i, 'M').format('MMMM');
+            vendite_mensili[nome_mese]=0;
         }
         for (var i = 0; i < dati.length; i++) {
             var vendita_corrente = dati[i]
             console.log(vendita_corrente);
-            var importo_vendita_corrente = vendita_corrente.amount;
+            var importo_vendita_corrente = parseInt(vendita_corrente.amount);
             console.log(importo_vendita_corrente);
             var data_corrente = moment(vendita_corrente.date, "DD/M/YYYY");
             console.log(data_corrente);
@@ -96,7 +131,7 @@ $(document).ready(function() {
         for (var i = 0; i < dati.length; i++) {
             var vendita_corrente = dati[i]
             // console.log(vendita_corrente);
-            var importo_vendita_corrente = vendita_corrente.amount;
+            var importo_vendita_corrente = parseInt(vendita_corrente.amount);
             console.log(importo_vendita_corrente);
             var nome_corrente = vendita_corrente.salesman;
             console.log(nome_corrente);
